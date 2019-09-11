4cea230e100b88b8a0fae892c9062561c253a8cd
Roland Olbricht <roland.olbricht@gmx.de>
2019-09-11 17:07:49 +0200

    Improved service scripts to generate internal links.

--- helper_scripts/convert_recursively.sh
+++ helper_scripts/convert_recursively.sh
@@ -1,46 +1,44 @@
 #!/usr/bin/env bash
 
 SOURCE="$1"
-TARGET="$2/"`dirname "$1"`/`basename "$1" .md`.html
-SCRIPT_DIR=`dirname $0`
-TITLE=`cat <$1 | awk '{ if (substr($1,1,3) == "===") print last; last = $0; }'`
+TARGET="$2/"$(dirname "$1")/$(basename "$1" .md).html
+PARENT_AUX_TARGET="$2/"$(dirname "$1")/../index.aux
+AUX_TARGET="$2/"$(dirname "$1")/index.aux
+SCRIPT_DIR=$(dirname $0)
+TITLE=$(cat <$1 | awk '{ if (substr($1,1,3) == "===") print last; last = $0; }')
 
-DIRPREFIX=
-if [[ `basename "$1"` == "index.md" ]]; then
-  DIRPREFIX="../"
+if [[ $(basename "$1") == "index.md" ]]; then
   if [[ $(dirname $1) == "de" ]]; then
-    declare -x TR_PREVIOUS="zurück"
     declare -x TR_NEXT="weiter"
     declare -x TR_TOC="Inhalt"
   elif [[ $(dirname $1) == "en" ]]; then
-    declare -x TR_PREVIOUS="prev"
     declare -x TR_NEXT="next"
     declare -x TR_TOC="Content"
   elif [[ $(dirname $1) == "fr" ]]; then
-    declare -x TR_PREVIOUS="arrière"
     declare -x TR_NEXT="prochaine"
     declare -x TR_TOC="Sommaire"
   elif [[ -z $TR_PREVIOUS || -z $TR_NEXT ]]; then
-    declare -x TR_PREVIOUS="-1"
     declare -x TR_NEXT="+1"
     declare -x TR_TOC="[+]"
   fi
 fi
+
+DIRPREFIX="../"
 if [[ -n $3 ]]; then
-  PRED_PAGE="$3"
-  PRED_FILE=$(dirname "$1")"/${DIRPREFIX}$PRED_PAGE"
-  PRED_TITLE=$(cat <"$PRED_FILE" | awk '{ if (substr($1,1,3) == "===") print last; last = $0; }')
-fi
-if [[ -n $4 ]]; then
-  SUCC_PAGE="$4"
-  SUCC_FILE=$(dirname "$1")"/${DIRPREFIX}$SUCC_PAGE"
-  SUCC_TITLE=$(cat <"$SUCC_FILE" | awk '{ if (substr($1,1,3) == "===") print last; last = $0; }')
-fi
-if [[ -n $5 ]]; then
-  PARENT="$5"
+  PARENT="$3"
+else
+  PARENT="$TITLE"
+  DIRPREFIX=
 fi
 
 mkdir -p $(dirname $TARGET)
+
+if [[ $(basename "$1") == "index.md" ]]; then
+  rm -f "$AUX_TARGET"
+  for I in $(cat <$1 | grep -E '^\[LINK_STUB\]\(' | awk '{ print substr($0,13,length($0)-13); }'); do
+    echo "$I $(cat <"$(dirname $1)/$I" | awk '{ if (substr($1,1,3) == "===") print last; last = $0; }')" >>$AUX_TARGET
+  done
+fi
 
 echo '<?xml version="1.0" encoding="UTF-8"?>' >$TARGET
 echo '<html xmlns="http://www.w3.org/1999/xhtml">' >>$TARGET
@@ -48,6 +46,7 @@ echo '<head><meta charset="utf-8"/>' >>$TARGET
 echo '  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>' >>$TARGET
 echo '<style>' >>$TARGET
 echo 'pre { background-color:#ccffff; padding: 0.5em; counter-reset: line; }' >>$TARGET
+echo 'div[type=sibling] { text-indent:1em; }' >>$TARGET
 echo 'codeline::before { counter-increment: line; content: counter(line)" "; color: #99cccc; }' >>$TARGET
 echo '</style>' >>$TARGET
 echo '  <title>'$TITLE'</title>' >>$TARGET
@@ -56,10 +55,21 @@ echo '<body style="font-family:sans-serif; max-width:32em">' >>$TARGET
 echo >>$TARGET
 
 {
-  echo $TR_TOC;
-  cat <$1 | awk -f helper_scripts/get_internal_anchors.awk;
+  if [[ -f $PARENT_AUX_TARGET ]]; then
+    if [[ $(basename "$1") == "index.md" ]]; then
+      cat <$PARENT_AUX_TARGET | awk '{ if ($1 == "'$(basename $(dirname "$1"))'/index.md") print "F"substr($0,index($0," ")); else print "u "$0; }'
+    else
+      cat <$PARENT_AUX_TARGET | awk '{ if ($1 == "'$(basename $(dirname "$1"))'/index.md") print "p"substr($0,index($0," ")); else print "u "$0; }'
+    fi
+  fi
+  if [[ -f $AUX_TARGET ]]; then
+    cat <$AUX_TARGET | awk '{ if ($1 == "'$(basename "$1")'") print "f "substr($0,index($0," ")); else print; }'
+  fi
+  echo "$TR_TOC"
+  echo "$TR_NEXT"
+  cat <$1 | awk -f "$SCRIPT_DIR/get_internal_anchors.awk";
   echo;
-  { 
+  {
     cat <$1 | awk -f "$SCRIPT_DIR/extract_code.awk" \
     | hexdump -v -e '/1 "%02X "' -e '/1 "%_p\n"' | awk -f "$SCRIPT_DIR/to_cgi.awk";
     echo;
@@ -68,23 +78,12 @@ echo >>$TARGET
     if [[ -n $PARENT ]]; then
       echo "x[$PARENT](${DIRPREFIX}index.md)  x"
     fi
-    if [[ -n $PRED_PAGE ]]; then
-      echo "x$TR_PREVIOUS: [$PRED_TITLE](${DIRPREFIX}$PRED_PAGE)  x"
-    fi
-    if [[ -n $SUCC_PAGE ]]; then
-      echo "x$TR_NEXT: [$SUCC_TITLE](${DIRPREFIX}$SUCC_PAGE)  x"
-    fi
-    if [[ -n $PARENT || -n $PRED_PAGE || -n $SUCC_PAGE ]]; then
+    if [[ -n $PARENT ]]; then
       echo 'xx'
       echo 'x---x'
       echo 'xx'
     fi
     sed 's/^/x/g' | sed 's/$/x/g'
-    if [[ -n $SUCC_PAGE ]]; then
-      echo
-      echo 'x---x'
-      echo "x$TR_NEXT: [$SUCC_TITLE](${DIRPREFIX}$SUCC_PAGE)  x"
-    fi
   } | while read LINE
   do
     if [[ ${LINE:1:12} == "[LINK_STUB](" ]]; then
@@ -98,21 +97,16 @@ echo >>$TARGET
     | markdown \
     | awk -f "$SCRIPT_DIR/tag_each_code_line.awk" \
     | sed 's/href=\"https:/target=\"_blank\" rel=\"noopener\" href=\"https:/g';
-} | awk -f helper_scripts/inject_local_toc.awk >>$TARGET
+} | awk -f "$SCRIPT_DIR/inject_local_toc.awk" >>$TARGET
 
 echo >>$TARGET
 echo '</body>' >>$TARGET
 echo '</html>' >>$TARGET
 
-LAST=
-LASTBUTONE=
 for I in $(cat <$1 | grep -E '^\[LINK_STUB\]\(' | awk '{ print substr($0,13,length($0)-13); }'); do
-  if [[ -n $LAST ]]; then
-    $0 "$(dirname $1)/$LAST" "$2" "$LASTBUTONE" "$I" "$TITLE"
-  fi
-  LASTBUTONE="$LAST"
-  LAST="$I"
+  $0 "$(dirname $1)/$I" "$2" "$PARENT"
 done
-if [[ -n $LAST ]]; then
-  $0 "$(dirname $1)/$LAST" "$2" "$LASTBUTONE" "" "$TITLE"
+
+if [[ $(basename "$1") == "index.md" ]]; then
+  rm -f "$AUX_TARGET"
 fi
--- helper_scripts/inject_local_toc.awk
+++ helper_scripts/inject_local_toc.awk
@@ -2,28 +2,92 @@
   if (!mode)
   {
     if ($0 == "")
-      mode = "seek";
-    else if (h3 == "")
-      h3 = $0;
-    else
+      mode = "seek_file";
+    else if (index($0," ") > 0)
     {
-      pos = index($0," ");
-      toc_lines = toc_lines "<div type=\"subsection\"><a href=\""substr($0,1,pos-1)"\">"substr($0,pos+1)"</a></div>\n";
+      if (substr($0,1,1) == "#")
+      {
+        pos = index($0," ");
+        toc_lines = toc_lines "<div type=\"subsection\"><a href=\""substr($0,1,pos-1)"\">"substr($0,pos+1)"</a></div>\n";
+      }
+      else
+      {
+        pos = index($0," ");
+        if ($1 == "f")
+        {
+          external_nav = external_nav "<div type=\"sibling\"><strong>"substr($0,pos+1)"</strong></div>\n";
+          succ_trigger = 1;
+        }
+        else if ($1 == "p")
+        {
+          parent_prefix = parent_suffix "<div type=\"parent\"><strong><a href=\"index.html\">"substr($0,pos+1)"</a></strong></div>\n";
+          parent_suffix = "";
+          succ_trigger = 1;
+        }
+        else if ($1 == "F")
+        {
+          parent_prefix = parent_suffix "<div type=\"parent\"><strong>"substr($0,pos+1)"</strong></div>\n";
+          parent_suffix = "";
+          succ_trigger = 2;
+        }
+        else if ($1 == "u")
+        {
+          rem = substr($0,pos+1);
+          pos = index(rem," ");
+          link = "<a href=\"../"substr(rem,1,pos-4)".html\">"substr(rem,pos+1)"</a>";
+          parent_suffix = parent_suffix "<div type=\"parent\">" link "</div>\n";
+          if (succ_trigger == 1)
+          {
+            succ_trigger = 0;
+            succ = link;
+          }
+        }
+        else
+        {
+          link = "<a href=\""substr($0,1,pos-4)".html\">"substr($0,pos+1)"</a>";
+          external_nav = external_nav "<div type=\"sibling\">" link "</div>\n";
+          if (succ_trigger > 0)
+          {
+            succ_trigger = 0;
+            succ = link;
+          }
+        }
+      }
     }
+    else if (tr_h3 == "")
+      tr_h3 = $0;
+    else if (tr_succ == "")
+      tr_succ = $0;
   }
   else
   {
-    if (mode == "seek" && substr($0,1,11) == "<p><a name=")
+    if (mode == "seek_file" && substr($0,1,3) == "<hr")
     {
       print "<nav>";
-      print "<h3>"h3"</h3>";
+      print parent_prefix;
+      print external_nav;
+      print "</nav>";
+      print parent_suffix;
+      mode = "seek_toc";
+    }
+    if (mode == "seek_toc" && substr($0,1,11) == "<p><a name=")
+    {
+      print "<nav>";
+      print "<h3>"tr_h3"</h3>";
       print "";
-      printf "%s",toc_lines;
+      print toc_lines;
       print "</nav>";
       print "";
 
       mode = "done";
     }
     print;
+  }
+}
+END {
+  if (succ != "")
+  {
+    print "<hr/>";
+    print "<p>" tr_succ ": " succ "</p>";
   }
 }
